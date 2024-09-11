6a5cecd2044e219ca58b9c4bd6e0d1ff91ea59c1
TechRider <techrider@gmail.com>
2024-09-11 17:16:17 -0500

    Added CRUD routes for animal endpoint

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ const app = express();
 const port = 3000;
 const animalRoutes = require("./routes/animal");
 const mongoose = require("mongoose");
+
+
 require("dotenv").config();
-app.use(parser.urlencoded({ extended: false })); //permite leer los datos que vienen en la petición
-app.use(parser.json()); // transforma los datos a formato JSON //Gestión de las rutas usando el middleware
-app.use("/api", animalRoutes);
-app.use(express.json()); //Conexión a la base de datos
+app.use(parser.urlencoded({ extended: false })); 
+
+//permite leer los datos que vienen en la petición
+app.use(parser.json()); 
+
+// transforma los datos a formato JSON //Gestión de las rutas usando el middleware
+app.use("/api/animals", animalRoutes);
+app.use(express.json()); 
+
+//Conexión a la base de datos
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("Conexión exitosa"))
